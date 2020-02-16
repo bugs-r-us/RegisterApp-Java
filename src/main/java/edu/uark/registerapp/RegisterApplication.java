fde9149662424cf6b3d1b3ef04b739440188bf0d
afrye1997 <allisonfrye97@gmail.com>
2020-02-16 13:09:57 -0600

    Frye Commit

--- src/main/java/edu/uark/registerapp/RegisterApplication.java
+++ src/main/java/edu/uark/registerapp/RegisterApplication.java
@@ -3,6 +3,7 @@ package edu.uark.registerapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//ALLISONS COMMIT
 @SpringBootApplication
 public class RegisterApplication {
 
