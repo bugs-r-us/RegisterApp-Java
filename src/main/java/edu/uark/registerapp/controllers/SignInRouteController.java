0ac89b2d03ef54c85fb82fb47c212441821facad
RyanRau <ryanzrau@gmail.com>
2020-03-02 14:40:14 -0600

    Cleaning stuff up

--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import edu.uark.registerapp.commands.employees.ActiveEmployeeExistsQuery;
-import edu.uark.registerapp.commands.employees.EmployeeQuery;
 import edu.uark.registerapp.commands.employees.EmployeeSignInCommand;
 import edu.uark.registerapp.controllers.enums.QueryParameterMessages;
 import edu.uark.registerapp.controllers.enums.QueryParameterNames;
@@ -32,8 +31,6 @@ public class SignInRouteController extends BaseRouteController {
 
 			String employeeId = queryParameters.get("employeeId");
 			if (employeeId != null){
-				//http://localhost:8080/?employeeId=00582 figure out
-
 				modelAndView.addObject("employeeId", employeeId);
 			}
 
@@ -61,7 +58,6 @@ public class SignInRouteController extends BaseRouteController {
 
 	@RequestMapping(value= "/signIn", method = RequestMethod.POST)
 	public ModelAndView performSignIn(
-		// TODO: Define an object that will represent the sign in request and add it as a parameter here
 		EmployeeSignIn employeeSignIn,
 		HttpServletRequest request
 	) 
@@ -78,29 +74,8 @@ public class SignInRouteController extends BaseRouteController {
 						REDIRECT_PREPEND.concat(ViewNames.SIGN_IN.getRoute()))
 						.addObject(ViewModelNames.ERROR_MESSAGE.getValue(), QueryParameterMessages.SIGN_IN_ERROR.getMessage());
 		}
-
-		// try{
-		// Employee employee = employeeQuery.
-		// = this.employeeSignInCommand.setapiEmployeeSignIn(employeeSignIn).
-		// 						  .findEmployee();
-	
-		// new ActiveUserEntity(employee, request.getRequestedSessionId());
-	
-		// return new ModelAndView(
-		// 	REDIRECT_PREPEND.concat(
-		// 		ViewNames.MAIN_MENU.getRoute()));
-		// }
-		// catch(final Exception e)
-		// {
-		// 	//TODO: SEND BACK AN ERROR MESSAGE 	
-		// 	return new ModelAndView(
-		// 		REDIRECT_PREPEND.concat(
-		// 			ViewNames.SIGN_IN.getRoute()));
-		// }
-
 	}
-	@Autowired
-	private EmployeeQuery employeeQuery;
+
 	@Autowired
 	private ActiveEmployeeExistsQuery activeEmployeeExistsQuery;
 	@Autowired
