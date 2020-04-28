27a4dcca7705dd0c6e26e250e82e20c6c86d532c
RyanRau <ryanzrau@gmail.com>
2020-04-27 20:31:40 -0500

    fix

--- src/main/resources/static/scripts/productDetail.js
+++ src/main/resources/static/scripts/productDetail.js
@@ -174,7 +174,7 @@ function deleteActionClick(event) {
 		deleteActionElement.disabled = false;
 
 		if (isSuccessResponse(callbackResponse)) {
-			window.location.replace("/");
+			window.location.replace("/mainMenu");
 		}
 	});
 };
